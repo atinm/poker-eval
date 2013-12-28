0ab9d3c54ddc253ea440e58df3dd013a8f72fa7a
atinm <atinm.dev@gmail.com>
2013-12-27 20:40:49 -0500

    Explain conversion between pokersource and Kev/2+2 cards

--- include/inlines/eval_omaha.h
+++ include/inlines/eval_omaha.h
@@ -136,6 +136,41 @@ StdDeck_OmahaHi_EVAL(StdDeck_CardMask hole, StdDeck_CardMask board,
 }
 
 /* Evaluate an omaha hand for high only, using 2+2 evaluator.  Return nonzero on error. */
+/* 
+       Kev/2+2:
+       2c =  1    2d =  2    2h =  3    2s =  4
+       3c =  5    3d =  6    3h =  7    3s =  8
+       4c =  9    4d = 10    4h = 11    4s = 12
+       5c = 13    5d = 14    5h = 15    5s = 16
+       6c = 17    6d = 18    6h = 19    6s = 20
+       7c = 21    7d = 22    7h = 23    7s = 24
+       8c = 25    8d = 26    8h = 27    8s = 28
+       9c = 29    9d = 30    9h = 31    9s = 32
+       Tc = 33    Td = 34    Th = 35    Ts = 36
+       Jc = 37    Jd = 38    Jh = 39    Js = 40
+       Qc = 41    Qd = 42    Qh = 43    Qs = 44
+       Kc = 45    Kd = 46    Kh = 47    Ks = 48
+       Ac = 49    Ad = 50    Ah = 51    As = 52
+
+       Poker source and Kev order the suits in differently, so after
+       my conversion of pokersource card index to Kev index:
+       card = (4 * StdDeck_RANK(playerCards[index])) + StdDeck_SUIT(playerCards[index]) + 1
+       gives me (this works for poker as suits aren't ordered)
+       
+       2h =  1    2d =  2    2c =  3    2s =  4
+       3h =  5    3d =  6    3c =  7    3s =  8
+       4h =  9    4d = 10    4c = 11    4s = 12
+       5h = 13    5d = 14    5c = 15    5s = 16
+       6h = 17    6d = 18    6c = 19    6s = 20
+       7h = 21    7d = 22    7c = 23    7s = 24
+       8h = 25    8d = 26    8c = 27    8s = 28
+       9h = 29    9d = 30    9c = 31    9s = 32
+       Th = 33    Td = 34    Tc = 35    Ts = 36
+       Jh = 37    Jd = 38    Jc = 39    Js = 40
+       Qh = 41    Qd = 42    Qc = 43    Qs = 44
+       Kh = 45    Kd = 46    Kc = 47    Ks = 48
+       Ah = 49    Ad = 50    Ac = 51    As = 52
+*/
 
 static inline int
 StdDeck_OmahaHi_EVAL_LUT(StdDeck_CardMask hole, StdDeck_CardMask board,
